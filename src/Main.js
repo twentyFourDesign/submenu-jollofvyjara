5f0cab460965c3ab31bb856c12f563819f03c074
Devband777 <world.devband777@gmail.com>
2023-07-15 00:26:07 +0300

    Create sub menu page

---
+++ src/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import FoodDetails from "./component/FoodDetails";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import MachineInfoComponent from "./component/MachineInfoComponent";
+import TakeOutDeliveryComponent from "./component/TakeOutDeliveryComponent";
+import MenuExternalEventCreating from "./component/MenuExternalEventCreating";
+import Menu from "./component/Menu";
+
+function Main() {
+  return (
+    <div>
+      <Header />
+      <MenuExternalEventCreating />
+      {/* <Menu /> */}
+      <TakeOutDeliveryComponent />
+      <MachineInfoComponent />
+      <FoodDetails />
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/component/MenuExternalEventCreating.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import useWindowWidth from "./width";
+import CardDetails from "./CardDetails";
+
+import Masonry from "react-smart-masonry";
+const breakpoints = { mobile: 600, tablet: 700, desktop: 1100 };
+
+export default function MenuExternalEventCreating() {
+  const [menu, setMenu] = useState([]);
+  const [menu1, setMenu1] = useState([]);
+  const [filteredMenu, setFilteredMenu] = useState({});
+
+  const [loading, setLoading] = useState(false);
+  const currWidth = useWindowWidth();
+
+  const fixCard = (data) => {
+    setMenu1(data);
+    const arry = [...data];
+    const firstElement = arry?.[0];
+    const lastElement = arry?.[arry?.length - 4];
+    arry?.pop();
+    arry.splice(3, 0, firstElement);
+    arry.splice(10, 0, lastElement);
+    arry?.shift();
+    setMenu(arry);
+  };
+
+  useLayoutEffect(() => {
+    setLoading(true);
+    fetch(`https://sabis.jollofbyjara.com/api/`)
+      .then((response) => response.json())
+      .then((data) => {
+        fixCard(data.data);
+      })
+      .catch(() => setMenu([]))
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    if (currWidth <= 1100) {
+      const arry = [...menu1];
+      const firstElement = menu1?.[0];
+      arry.splice(2, 0, firstElement);
+      setMenu(arry);
+    } else {
+      fixCard(menu1);
+    }
+  }, [currWidth, menu1]);
+
+  useEffect(() => {
+    if (menu1?.length) {
+      let menuItem = menu1.find(
+        (item) =>
+          item.title.toUpperCase() ===
+          "EXTERNAL EVENT CATERING (24HRS NOTICE REQUIRED)"
+      );
+      console.log(menuItem);
+      setFilteredMenu(menuItem);
+    }
+  }, [menu1?.length]);
+
+  if (loading) {
+    return (
+      <div className="testing">
+        <h2 className="data-notfound">Loading...</h2>
+      </div>
+    );
+  }
+  return (
+    <div className="testing">
+      {/* <Masonry
+        autoArrange={true}
+        breakpoints={breakpoints}
+        columns={{ mobile: 1, tablet: 1, desktop: 1 }}
+        gap={{ mobile: 20, tablet: 30, desktop: 40 }}
+      > */}
+        {filteredMenu ? (
+          <CardDetails title={filteredMenu?.title} items={filteredMenu?.data} />
+        ) : (
+          <h2 className="data-notfound">Menu Not Found</h2>
+        )}
+      {/* </Masonry> */}
+    </div>
+  );
+}
