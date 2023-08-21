a508b9d2afb83ab7de879e37d3079a8302b8986b
Talha <talhahaider074@gmail.com>
2023-08-21 23:54:46 +0500

    responsive issue solved

--- src/component/CardDetails.js
+++ src/component/CardDetails.js
@@ -5,7 +5,7 @@ const breakpoints = { mobile: 600, tablet: 700, desktop: 1100 };
 const List = ({ items, title }) => {
   const [openModal, setOpenModal] = useState(false);
   const [selectedData, setSelectedData] = useState({});
-
+  console.log(selectedData.image)
   const handleItemClick = (item) => {
     setOpenModal(!openModal);
     setSelectedData(item);
@@ -15,8 +15,6 @@ const List = ({ items, title }) => {
     setOpenModal(!openModal);
   };
 
-  
-
   useEffect(() => {
     if (openModal) {
       document.body.style.overflow = "hidden";
@@ -24,51 +22,49 @@ const List = ({ items, title }) => {
       document.body.style.overflow = "auto";
     }
   }, [openModal]);
+  console.log(selectedData)
+  console.log(selectedData?.title,"title")
+  console.log(selectedData?.image,"image")
+  console.log(selectedData?.price,"price")
+  console.log(selectedData?.description,"description")
 
   return (
     <div className="details-container">
-      <div className="details-header">{title}</div>
-      <div className="list-container">
-      <Masonry
-        autoArrange={true}
-        breakpoints={breakpoints}
-        columns={{ mobile: 1, tablet: 2, desktop: 3 }}
-        gap={{ mobile: 20, tablet: 30, desktop: 40 }}
-      >
-        {items?.map((item, index) => (
-          <div
-            key={index}
-            className="list-item"
-            onClick={() => handleItemClick(item)}
-          >
-            <div className="item-name">{item.title}</div>
-            <div className="item-line">
-              <hr></hr>
-            </div>
-            <div className="item-price">{item.price.toLocaleString()}</div>
-          </div>
-        ))}
-        </Masonry>
+      <h1 className="details-header">{title}</h1>
+      <div className="menuItems">
+        {
+          items?.map((item)=>{
+            return(
+              <div className="childMenu" onClick={() => handleItemClick(item)}>
+                <p className="food-name">{item?.title}</p>
+                <div className="hr-parent">
+                  <hr className="hrTag"/>
+                </div>
+                <p className="food-price">{item?.price?.toLocaleString()}</p>
+              </div>
+            )
+          })
+        }
       </div>
       {openModal && (
         <div className="popup">
           <div className="popup-inner">
             <div className="popup-header">
-              <div className="selected-item-name">{selectedData.title}</div>
+              <div className="selected-item-name">Title: {selectedData?.title}</div>
               <button className="popup-close" onClick={handleClose}>
                 &times;
               </button>
             </div>
             <div className="popup-content">
-              <div className="popup-price">{openModal.price}</div>
+              <div className="popup-price">Price: {selectedData?.price?.toLocaleString()}</div>
               <div className="popup-description">
                 <div className="selected-list-container">
                   <img
-                    src={selectedData.image}
+                    src={selectedData?.image}
                     className="select-item-image"
-                    alt={selectedData.title}
+                    alt={selectedData?.title}
                   />
-                  <p className="food-description">{selectedData.description}</p>
+                  <p className="food-description">Description: {selectedData?.description}</p>
                 </div>
               </div>
             </div>
@@ -80,3 +76,27 @@ const List = ({ items, title }) => {
 };
 
 export default List;
+
+
+{/* <div className="list-container">
+<Masonry
+  autoArrange={true}
+  breakpoints={breakpoints}
+  columns={{ mobile: 1, tablet: 2, desktop: 3 }}
+  gap={{ mobile: 20, tablet: 30, desktop: 40 }}
+>
+  {items?.map((item, index) => (
+    <div
+      key={index}
+      className="list-item"
+      onClick={() => handleItemClick(item)}
+    >
+      <div className="item-name">{item.title}</div>
+      <div className="item-line">
+        <hr></hr>
+      </div>
+      <div className="item-price">{item.price.toLocaleString()}</div>
+    </div>
+  ))}
+  </Masonry>
+</div> */}
